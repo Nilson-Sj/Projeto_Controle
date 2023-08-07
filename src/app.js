f9ee81f2a31f115cddf44f044a21c07e97200f6b
Nilson-Sj <nilsonjr180795@gmail.com>
2023-08-07 12:34:53 -0300

    deve listar as marcas

--- src/app.js
+++ src/app.js
@@ -4,4 +4,11 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
+app.get('/marcas', (req, res) => {
+  const marcas = [
+    { nome: 'BMW' },
+  ];
+  res.status(200).json(marcas);
+});
+
 module.exports = app;
