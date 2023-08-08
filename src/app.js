ae00f9759d30da380a83741304b5a5f1c1b194fa
Nilson-Sj <nilsonjr180795@gmail.com>
2023-08-08 08:45:32 -0300

    deve inserir marca com sucesso

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
 const app = require('express')();
 
+const bodyParser = require('body-parser');
+
+app.use (bodyParser.json());
+
 app.get('/', (req, res) => {
   res.status(200).send();
 });
 
 app.get('/marcas', (req, res) => {
   const marcas = [
-    { nome: 'BMW' },
+    { nome: 'Acer' },
+    { nome: 'Samsung' },
+    { nome: 'Dell' },
+    { nome: 'Asus' },
+    { nome: 'Lenovo' },
+    { nome: 'Apple' },
   ];
   res.status(200).json(marcas);
+});
+
+app.post('/marcas', (req, res) => {
+ res.status(201).json(req.body);
 });
 
 module.exports = app;
--- test/marcas.test.js
+++ test/marcas.test.js
@@ -6,7 +6,15 @@ test('Deve listar todas as marcas', () => {
   return request(app).get('/marcas')
   .then((res) => {
     expect(res.status).toBe(200);
-    expect(res.body).toHaveLength(1);
     expect(res.body[0]).toHaveProperty('nome');
+  });
+});
+
+test('Deve inserir marca com sucesso', () => {
+  return request(app).post('/marcas')
+  .send({ nome: 'Positivo' })
+  .then((res) => {
+    expect(res.status).toBe(201);
+    expect(res.body.nome).toBe('Positivo');
   });
 });
