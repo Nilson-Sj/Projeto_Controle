3b2cb6b495f7fce912eea2ec5038eb98f03008c4
Nilson-Sj <nilsonjr180795@gmail.com>
2023-08-08 10:43:52 -0300

    refatorei o codigo para usar banco de dados

--- src/routes/marcas.js
+++ src/routes/marcas.js
@@ -1,18 +1,12 @@
-module.exports = () => {
+module.exports = (app) => {
     const findAll = (req, res) => {
-        const marcas = [
-          { nome: 'Acer' },
-          { nome: 'Samsung' },
-          { nome: 'Dell' },
-          { nome: 'Asus' },
-          { nome: 'Lenovo' },
-          { nome: 'Apple' },
-        ];
-        res.status(200).json(marcas);
+        app.db('marca').select()
+        .then(result => res.status(200).json(result))
       };
 
-      const create = (req, res) => {
-       res.status(201).json(req.body);
+      const create = async (req, res) => {
+      const result = await app.db('marca').insert(req.body, '*');
+       res.status(201).json(result[0]);
       };
 
       return { findAll, create };
--- test/routes/marcas.test.js
+++ test/routes/marcas.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
 
-const app = require('../src/app');
+const app = require('../../src/app');
 
 test('Deve listar todas as marcas', () => {
   return request(app).get('/marcas')
   .then((res) => {
     expect(res.status).toBe(200);
-    expect(res.body[0]).toHaveProperty('nome');
+    expect(res.body.length).toBeGreaterThan(0);
   });
 });
 
