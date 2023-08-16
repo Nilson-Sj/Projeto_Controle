2a940b75c85b5a7df968c36aa69fbe471cc18957
Nilson-Sj <nilsonjr180795@gmail.com>
2023-08-16 09:05:07 -0300

    mudei o gerenciamento de erro

--- src/routes/marcas.js
+++ src/routes/marcas.js
@@ -1,31 +1,35 @@
 module.exports = (app) => {
-    const getAll = (req, res) => {
+    const getAll = (req, res, next) => {
         app.services.marca.getAll()
           .then(result => res.status(200).json(result))
+          .catch(err => next(err));
       };
 
-    const create = async (req, res) => {
+    const create = async (req, res, next) => {
       try {
       const result = await app.services.marca.save(req.body);
       return res.status(201).json(result[0]);
       } catch (err) {
-        return res.status(400).json({ error: err.message });
+        return next(err);
       };
     };
 
-      const getByName = (req, res) => {
+      const getByName = (req, res, next) => {
         app.services.marca.getByName({ nome: req.params.nome})
-          .then(result => res.status(200).json(result));
+          .then(result => res.status(200).json(result))
+          .catch(err => next(err));
       };
 
-      const update = (req, res) => {
+      const update = (req, res, next) => {
         app.services.marca.update(req.params.id, req.body)
-          .then(result => res.status(200).json(result[0]));
+          .then(result => res.status(200).json(result[0]))
+          .catch(err => next(err));
       };
 
-      const remove = (req, res) => {
+      const remove = (req, res, next) => {
         app.services.marca.remove(req.params.id)
-          .then(() => res.status(204).send());
+          .then(() => res.status(204).send())
+          .catch(err => next(err));
       }
 
       return { getAll, create, getByName, update, remove };
